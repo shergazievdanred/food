c6ba368677e58ce0bb27dd44ec511424225c2336
Daniyar <shergazievdan@gmail.com>
2021-08-11 15:49:16 +0600

    modal_window2_11.08.2021

--- js/script.js
+++ js/script.js
@@ -92,17 +92,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
      // Modal
 
-     const modalTrigger = document.querySelectorAll('[data-modal]'),
-     modal = document.querySelector('.modal'),
-     modalCloseBtn = document.querySelector('[data-close]');
+    const   modalTrigger = document.querySelectorAll('[data-modal]'),
+            modal = document.querySelector('.modal'),
+            modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }              
 
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', function() {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            // Либо вариант с toggle - но тогда назначить класс в верстке
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -126,4 +128,14 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    const modalTimerId = setTimeout(openModal,20000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
