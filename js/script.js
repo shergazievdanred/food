954099148d4431d0e08ffe53c98f54dcc29fb226
Daniyar <shergazievdan@gmail.com>
2022-02-04 17:22:08 +0600

    food-oxios-04.02.2022

--- js/script.js
+++ js/script.js
@@ -141,41 +141,100 @@ window.addEventListener('DOMContentLoaded', function() {
     }
     window.addEventListener('scroll', showModalByScroll);
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
+    // Используем классы для создание карточек меню
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 84.5;
+            this.changeToUAH(); 
         }
-        
-        return await res.json();
-    };
 
-    getResource('http://localhost:3000/menu')
-        .then(data => createCard(data));
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
+        }
 
-    function createCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) => {
+        render() {
             const element = document.createElement('div');
 
-            price = price * 84;
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
 
-            element.classList.add('menu__item');
             element.innerHTML = `
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle">${title}</h3>
-                <div class="menu__item-descr">${descr}</div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${price}</span> сом/день</div>
+                    <div class="menu__item-total"><span>${this.price}</span> сом/день</div>
                 </div>
             `;
-
-            document.querySelector('.menu .container').append(element);
-
-        });
+            this.parent.append(element);
+        }
     }
+
+    // const getResource = async (url) => {
+    //     const res = await fetch(url);
+
+    //     if (!res.ok) {
+    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //     }
+        
+    //     return await res.json();
+    // };
+//     getResource('http://localhost:3000/menu')
+//     .then(data => createCard(data));
+
+// function createCard(data) {
+//     data.forEach(({img, altimg, title, descr, price}) => {
+//         const element = document.createElement('div');
+
+//         price = price * 84;
+
+//         element.classList.add('menu__item');
+//         element.innerHTML = `
+//             <img src=${img} alt=${altimg}>
+//             <h3 class="menu__item-subtitle">${title}</h3>
+//             <div class="menu__item-descr">${descr}</div>
+//             <div class="menu__item-divider"></div>
+//             <div class="menu__item-price">
+//                 <div class="menu__item-cost">Цена:</div>
+//                 <div class="menu__item-total"><span>${price}</span> сом/день</div>
+//             </div>
+//         `;
+
+//         document.querySelector('.menu .container').append(element);
+
+//     });
+// }
+
+/**закомментировал предыдущий запрос */
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+            // data.forEach(({img, altimg, title, descr, price}) => {
+            //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            // });
+    //     });
+
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
     // Forms
 
     const forms = document.querySelectorAll('form');
